6f01cdbfc7419f90d3b562b49a615f42c78f099f
Angela Salem <angel.salem.008@gmail.com>
2024-01-07 16:06:44 +0200

    added navigation bar and started styling table page

--- php/homeService.php
+++ php/homeService.php
@@ -16,15 +16,15 @@ if ($conn->connect_error) {
 
 $sql = "SHOW TABLES";
 $result = $conn->query($sql);
-$cols = array();
+$tables = array();
 
 if ($result->num_rows > 0) {
    // output data of each row
    while($row = $result->fetch_assoc()) {
          $table = $row["Tables_in_cars"];
-      array_push($cols,$table);
+      array_push($tables,$table);
    }
-   echo json_encode($cols);
+   echo json_encode($tables);
 } else {
    echo "0 results";
 }
--- table.php
+++ table.php
@@ -4,9 +4,10 @@ if (!isset($_SESSION["user"])) {
 
     header("Location: registration.html");
 } else {
-    $table = $_GET["table"];
-}
+    $table = $_GET["table"];}
 ?>
+
+
 <html lang="en">
 
 <head>
@@ -14,18 +15,19 @@ if (!isset($_SESSION["user"])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <title>search car table</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="css/tableStyle.css">
+    <link rel="icons" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/tableStyle.css">
 
 </head>
 
 
 <body>
     <div class="navbar">
-        <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a>
-        <a href="#"><i class="fa fa-fw fa-search"></i> Search</a>
-        <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a>
-        <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
+        <ul id="tabList">
+            <li><a class="active" href="index.php"><i class="fa fa-fw fa-home"></i> Home</a></li>
+            
+        </ul>
     </div>
     <!-- <h1></h1> -->
 
@@ -36,8 +38,8 @@ if (!isset($_SESSION["user"])) {
 
 
     <!-- <embed type="text/html" src="registration.html" width="500" height="200"> -->
-    <!-- <div id="leftContainer">
-        <div id="res"></div>
+    <div id="leftContainer">
+        <!-- <div id="res"></div> -->
     </div>
 
     <div id="rightContainer"></div>
@@ -46,6 +48,24 @@ if (!isset($_SESSION["user"])) {
         $(document).ready(function() {
 
             var tableName = <?php echo json_encode($table, JSON_HEX_TAG); ?>;
+            $.ajax({
+                url: "php/homeService.php",
+                type: "POST",
+                success: function(data) {
+                    var tables = JSON.parse(data);
+                    while (tables.length > 0) {
+                        var tbl = titleCase(tables.pop());
+                        if (tbl == "Login ") {
+                            continue;
+                        }
+                        $("#tabList").append(buildTab(tbl, "table.php?table=" + tbl));
+                        $("#res").append("hee");
+                    }
+                },
+                error: function(data) {
+                    console.log(data);
+                }
+            });
 
             $("h1").html(titleCase(tableName) + " Table");
 
@@ -62,15 +82,21 @@ if (!isset($_SESSION["user"])) {
                     }
                 });
             });
+
+           
+
         });
 
-
+        function buildTab(title, link) {
+                var tab =  "<li><a href='" + link + "'>" + title + "</a></li>";
+                return tab;
+            }
 
         function titleCase(st) {
             return st.toLowerCase().split(" ").reduce((s, c) =>
                 s + "" + (c.charAt(0).toUpperCase() + c.slice(1) + " "), '');
         }
-    </script> -->
+    </script> 
 
 </body>
 
